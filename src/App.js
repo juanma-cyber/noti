a79245ad9df8ddaf6cf26cc59f8d16db9fe112c0
juanma-cyber <psilosybe11@gmail.com>
2022-12-26 16:21:41 +0100

    mundo commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/common/header/Header"
+import "./App.css"
+import Homepages from "./components/home/Homepages"
+import Footer from "./components/common/footer/Footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import SinglePage from "./components/singlepage/SinglePage"
+import Culture from "./components/culture/Culture"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={  <Homepages />} />
+          <Route path='/singlepage/:id' exact element={<SinglePage/>} />
+          <Route exact path='/culture' element={<Culture />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
